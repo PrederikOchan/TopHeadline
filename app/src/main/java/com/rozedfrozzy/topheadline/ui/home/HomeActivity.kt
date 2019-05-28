cddcc6b7b84a434a37f1c172d0946304898eb9b2
Roshan Din Firozi <roze.d.frozzy@gmail.com>
2019-05-28 15:21:36 +0700

    (+) add searchView

--- app/src/main/java/com/rozedfrozzy/topheadline/ui/home/HomeActivity.kt
+++ app/src/main/java/com/rozedfrozzy/topheadline/ui/home/HomeActivity.kt
@@ -2,12 +2,34 @@ package com.rozedfrozzy.topheadline.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.rozedfrozzy.topheadline.R
+import com.rozedfrozzy.topheadline.ui.search.SearchActivity
+import kotlinx.android.synthetic.main.activity_home.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when (item?.itemId) {
+        R.id.searchBtn -> {
+            startActivity<SearchActivity>()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/rozedfrozzy/topheadline/ui/search/SearchActivity.kt
+++ app/src/main/java/com/rozedfrozzy/topheadline/ui/search/SearchActivity.kt
@@ -3,11 +3,21 @@ package com.rozedfrozzy.topheadline.ui.search
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rozedfrozzy.topheadline.R
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        setSupportActionBar(appBarSearch)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setupSearchView()
+    }
+
+    private fun setupSearchView() {
+        searchBar.requestFocus()
     }
 }
